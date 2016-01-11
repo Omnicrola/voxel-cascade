82dd2362b441f1ac356ab70ef51fd0c7b2fcf38d
Eric Schreffler <omnicrola@gmail.com>
2016-01-10 19:47:28 -0500

    Added maven boilerplate for building with LWJGL

---
+++ src/main/java/com/omnicrola/voxel/main/VoxelCascade.java
@@ -0,0 +1,11 @@
+package com.omnicrola.voxel.main;
+
+/**
+ * Created by omnic on 1/10/2016.
+ */
+public class VoxelCascade {
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
