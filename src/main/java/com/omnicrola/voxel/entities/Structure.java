f32895e1d3d22bc85d066a4a210b3c8be1f5543e
Eric Schreffler <omnicrola+tards@gmail.com>
2016-02-28 21:44:41 -0500

    Fixed structures not being placed correctly (need to set physics location)

--- src/main/java/com/omnicrola/voxel/entities/Structure.java
+++ src/main/java/com/omnicrola/voxel/entities/Structure.java
@@ -1,5 +1,7 @@
 package com.omnicrola.voxel.entities;
 
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
 
 /**
@@ -8,5 +10,10 @@ import com.jme3.scene.Spatial;
 public class Structure extends AbstractGameEntity {
     public Structure(Spatial spatial) {
         super(spatial);
+    }
+
+    @Override
+    public void setLocation(Vector3f location) {
+        this.spatial.getControl(RigidBodyControl.class).setPhysicsLocation(location);
     }
 }
